abfc0342bbeba5b02fd9cb97cecc5757d9d96e00
Krzysiu <kheciek@gmail.com>
2017-09-30 21:31:03 +0200

    Jumping slide issue fixed

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(function() {
     }
     
     function changeSlide() {
-        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide());
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
     }
     
     setInterval(changeSlide, 2000);
